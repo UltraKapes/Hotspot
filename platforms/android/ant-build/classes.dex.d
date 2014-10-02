3bd3b2d372aba98ea168ad067d93cadf3a121208
UltraKapes <kapes1980@hotmail.com>
2014-10-02 17:48:49 +1000

    fixed splash

--- platforms/android/ant-build/classes.dex.d
+++ platforms/android/ant-build/classes.dex.d
@@ -1,14 +1,14 @@
-c:\Users\shane\Documents\GitHub\Hotspot\platforms\android\ant-build\classes.dex : \
-c:\Users\shane\Documents\GitHub\Hotspot\platforms\android\ant-build\classes\com\phonegap\hotsopt\BuildConfig.class \
-c:\Users\shane\Documents\GitHub\Hotspot\platforms\android\ant-build\classes\com\phonegap\hotsopt\HOTSPOT.class \
-c:\Users\shane\Documents\GitHub\Hotspot\platforms\android\ant-build\classes\com\phonegap\hotsopt\R$attr.class \
-c:\Users\shane\Documents\GitHub\Hotspot\platforms\android\ant-build\classes\com\phonegap\hotsopt\R$drawable.class \
-c:\Users\shane\Documents\GitHub\Hotspot\platforms\android\ant-build\classes\com\phonegap\hotsopt\R$string.class \
-c:\Users\shane\Documents\GitHub\Hotspot\platforms\android\ant-build\classes\com\phonegap\hotsopt\R$xml.class \
-c:\Users\shane\Documents\GitHub\Hotspot\platforms\android\ant-build\classes\com\phonegap\hotsopt\R.class \
-c:\Users\shane\Documents\GitHub\Hotspot\platforms\android\ant-build\classes\org\apache\cordova\devicemotion\AccelListener$1.class \
-c:\Users\shane\Documents\GitHub\Hotspot\platforms\android\ant-build\classes\org\apache\cordova\devicemotion\AccelListener.class \
-c:\Users\shane\Documents\GitHub\Hotspot\platforms\android\ant-build\classes\org\apache\cordova\deviceorientation\CompassListener$1.class \
-c:\Users\shane\Documents\GitHub\Hotspot\platforms\android\ant-build\classes\org\apache\cordova\deviceorientation\CompassListener.class \
-c:\Users\shane\Documents\GitHub\Hotspot\platforms\android\ant-build\classes\org\apache\cordova\splashscreen\SplashScreen.class \
-c:\Users\shane\Documents\GitHub\Hotspot\platforms\android\ant-build\dexedLibs\classes-ffe28a91b31c81c4a9cfa03bddadf61a.jar \
+c:\Users\Shane\git\Hotspot\platforms\android\ant-build\classes.dex : \
+c:\Users\Shane\git\Hotspot\platforms\android\ant-build\classes\com\phonegap\hotsopt\BuildConfig.class \
+c:\Users\Shane\git\Hotspot\platforms\android\ant-build\classes\com\phonegap\hotsopt\HOTSPOT.class \
+c:\Users\Shane\git\Hotspot\platforms\android\ant-build\classes\com\phonegap\hotsopt\R$attr.class \
+c:\Users\Shane\git\Hotspot\platforms\android\ant-build\classes\com\phonegap\hotsopt\R$drawable.class \
+c:\Users\Shane\git\Hotspot\platforms\android\ant-build\classes\com\phonegap\hotsopt\R$string.class \
+c:\Users\Shane\git\Hotspot\platforms\android\ant-build\classes\com\phonegap\hotsopt\R$xml.class \
+c:\Users\Shane\git\Hotspot\platforms\android\ant-build\classes\com\phonegap\hotsopt\R.class \
+c:\Users\Shane\git\Hotspot\platforms\android\ant-build\classes\org\apache\cordova\devicemotion\AccelListener$1.class \
+c:\Users\Shane\git\Hotspot\platforms\android\ant-build\classes\org\apache\cordova\devicemotion\AccelListener.class \
+c:\Users\Shane\git\Hotspot\platforms\android\ant-build\classes\org\apache\cordova\deviceorientation\CompassListener$1.class \
+c:\Users\Shane\git\Hotspot\platforms\android\ant-build\classes\org\apache\cordova\deviceorientation\CompassListener.class \
+c:\Users\Shane\git\Hotspot\platforms\android\ant-build\classes\org\apache\cordova\splashscreen\SplashScreen.class \
+c:\Users\Shane\git\Hotspot\platforms\android\ant-build\dexedLibs\classes-fd65543c9dcf3740f869f4811ec817a1.jar \
