92c1ba108f16d3246b7a5a557487c4587efea2a5
UltraKapes <kapes1980@hotmail.com>
2014-10-11 00:19:35 +1000

    uploads page

--- platforms/android/ant-build/HOTSPOT-debug-unaligned.apk.d
+++ platforms/android/ant-build/HOTSPOT-debug-unaligned.apk.d
@@ -1,4 +1,4 @@
-c:\Users\Shane\git\Hotspot\platforms\android\ant-build\HOTSPOT-debug-unaligned.apk : \
-c:\Users\Shane\git\Hotspot\platforms\android\ant-build\HOTSPOT.ap_ \
-c:\Users\Shane\git\Hotspot\platforms\android\ant-build\classes.dex \
-C:\Users\Shane\git\Hotspot\platforms\android\CordovaLib\ant-build\classes.jar \
+c:\Users\shane\Documents\GitHub\Hotspot\platforms\android\ant-build\HOTSPOT-debug-unaligned.apk : \
+c:\Users\shane\Documents\GitHub\Hotspot\platforms\android\ant-build\HOTSPOT.ap_ \
+c:\Users\shane\Documents\GitHub\Hotspot\platforms\android\ant-build\classes.dex \
+C:\Users\shane\Documents\GitHub\Hotspot\platforms\android\CordovaLib\ant-build\classes.jar \
